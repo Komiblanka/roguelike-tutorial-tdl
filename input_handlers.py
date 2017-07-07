2c0a219f6fb05fe2a25dbecdc802b286ba922200
Alberto Hervalejo <ahervalejo@gmail.com>
2017-07-08 01:10:27 +0200

    First week!

---
+++ input_handlers.py
@@ -0,0 +1,23 @@
+import tdl
+
+def handle_keys(user_input):
+    # Movement keys
+    if user_input.key == "UP":
+        return {"move": (0, -1)}
+    elif user_input.key == "DOWN":
+        return {"move": (0, 1)}
+    elif user_input.key == "LEFT":
+        return {"move": (-1, 0)}
+    elif user_input.key == "RIGHT":
+        return {"move": (1, 0)}
+
+    if user_input.key == "ENTER" and user_input.alt:
+        # Alt + enter toggle full screen
+        return {"fullscreen": True}
+
+    elif user_input.key == "ESCAPE":
+        return {"exit": True}
+        
+    # No Key was pressed
+    return {} 
+
